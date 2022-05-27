6b336ad8e2400c304948de61ab0c5cdbad9c43b5
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-05-27 13:20:17 -0400

    ensayo

--- AE3/EnsayoEvaluacion.py
+++ AE3/EnsayoEvaluacion.py
@@ -45,13 +45,13 @@ while opcion != 4:
         comuna      = input("ingrese su comuna       : ")
         correo      = input("ingrese su correo       : ")
 
-        if correo.count("@") != 0:
+        if correo.count("@") != 1:
             print("Error: Correo no es válido")
             input("Presione enter para continuar....")
             continue
         
         try:
-            edad = int(input("Ingrese rut:"))
+            edad = int(input("Ingrese edad:"))
 
             if edad < 0 or edad > 110:
                 print("Edad fuera de rango")
@@ -63,7 +63,7 @@ while opcion != 4:
 
         genero = input("ingrese su genero(F o M): ")
         genero = genero.upper()
-        if genero != 'F' or genero != "M":
+        if genero != "F" and genero != "M":
             print("Error: Género no es válido")
             input("Presione enter para continuar....")
             continue
@@ -85,9 +85,63 @@ while opcion != 4:
         cliente = [rut, nombre, direccion, comuna, correo, edad,genero,celular,tipo, suscrito]
         clientes.append(cliente)
     elif opcion == 2:
-        print("Seleccionó la opción 1")
+        print("Seleccionó la opción 2")
+
+        try:
+            rut = int(input("Ingrese rut:"))
+
+            if rut < 4000000 or rut > 99999999:
+                print("Rut fuera de rango")
+                raise("Error de rango")
+        except:
+            print("Error: Rut no es válido")
+            input("Presione enter para continuar....")
+            continue
+        fueEncontrado = False
+        for cliente in clientes:
+            if cliente[0] == rut:
+                cliente.append('27-05-2022')
+                print("Usuario encontrado")
+                fueEncontrado = True
+                break
+        
+        if not fueEncontrado:
+            print("El rut no fue encontrado")
+
     elif opcion == 3:
-        print("Seleccionó la opción 1")
+        print("Seleccionó la opción 3")
+        try:
+            rut = int(input("Ingrese rut:"))
+
+            if rut < 4000000 or rut > 99999999:
+                print("Rut fuera de rango")
+                raise("Error de rango")
+        except:
+            print("Error: Rut no es válido")
+            input("Presione enter para continuar....")
+            continue
+
+        clienteEncontrado = []
+        for cliente in clientes:
+            if cliente[0] == rut:
+                clienteEncontrado = cliente
+                break
+        
+        if  len(clienteEncontrado) == 0:
+            print("El rut no fue encontrado")
+        else:
+            print("*****  Datos del cliente ******")
+            print("Rut           :", clienteEncontrado[0])
+            print("Nombre           :", clienteEncontrado[1])
+            print("Dirección        :", clienteEncontrado[2])
+            print("Comuna           :", clienteEncontrado[3])
+            print("Correo           :", clienteEncontrado[4])
+            print("Edad             :", clienteEncontrado[5])
+            print("Genero           :", clienteEncontrado[6])
+            print("Celular          :", clienteEncontrado[7])
+            print("Tipo             :", clienteEncontrado[8])
+            print("Fecha Suscripción:", clienteEncontrado[10])
+
     elif opcion == 4:
         print("Aplicación cerrada")
 
---
+++ AE3/genero.py
@@ -0,0 +1,5 @@
+
+genero = input("ingrese su genero(F o M): ")
+genero = genero.upper()
+if genero != "F" and genero != "M":
+    print("Error: Género no es válido")
