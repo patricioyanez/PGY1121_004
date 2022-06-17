d40377ce723cb536d9f5e8587597b141d7a35ac3
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2022-06-17 19:21:46 -0400

    Create ejercicioFuncionMenu2Sol.py

---
+++ EA4/ejercicioFuncionMenu2Sol.py
@@ -0,0 +1,108 @@
+# instalar e importar numpy ==> pip install numpy
+import numpy as nu
+casillero = nu.array([ ["", "", ""], 
+                        ["", "", ""], 
+                        ["", "", ""] ], dtype = object)
+
+# definir funciones
+def arrendar(casillero):
+    print("=== Arrendar casillero ===")
+    print("1.- nivel 1 $ 10.000")
+    print("2.- nivel 2 $  5.000")
+    print("3.- nivel 3 $  2.000")
+
+    try:
+        opcion = int(input("Ingrese nivel del casillero: ")) # fila
+        fila = opcion - 1
+        casillerosDisponibles(casillero, fila)
+        nroCasillero = int(input("Ingrese nivel del casillero: ")) # columna
+        columna = nroCasillero - 1
+        nombre = input("Ingrese el nombre          : ")
+        casillero[fila, columna] = nombre
+#        print(casillero)
+    except:
+        print("=== Error: Opción no válida")
+        input("Presione enter para continuar...")        
+
+
+def casillerosDisponibles(casillero, fila):
+    print("Casilleros disponibles de la fila ", fila+1)
+    nroCasillero = 1
+    for columna in casillero[fila]:
+        if columna == "":
+            print("Casillero ", nroCasillero)
+        nroCasillero += 1
+
+
+def mostrarUbicaciones(casillero):
+    print("*** Disponibilidad de casilleros ***")
+    filas = ""
+    nroCasillero = 1
+    valor = ""
+    for fila in casillero:
+        for columna in fila:
+            if columna == "":
+                valor = str(nroCasillero)
+            else:
+                valor = "X"
+            filas += valor + " " 
+            nroCasillero += 1
+        filas += "\n"
+    print(filas)
+    input("Presione enter para continuar...") 
+
+
+def mostrarNombre(casillero):
+    print("Clientes de los casilleros")
+    listado = ""
+    nroCasillero = 1
+    for fila in casillero:
+        for columna in fila:
+ #           if columna != "":
+            print("casillero: ", nroCasillero, "nombre:", columna)
+            nroCasillero += 1
+    input("Presione enter para volver al menú...")
+
+def calcularGanancias(casillero):
+    total = 0
+    fil = 1
+    for fila in casillero:
+        for columna in fila:
+            if columna != "":
+                if fil == 1:
+                    total += 10000
+                elif fil == 2:
+                    total += 5000
+                elif fil == 3:
+                    total += 2000            
+        fil += 1
+    print("Total de ganancias:", total)
+    input("Presione enter para volver al menú...")
+
+# definir aplicacion
+opcion= 0
+listadoDeOpciones = ["1", "2", "3", "4", "5"]
+while opcion != "5":
+    print("===== Arriendo de casilleros =====")
+    print("1.- Arrendar")
+    print("2.- Mostrar ubicaciones disponibles")
+    print("3.- Listar nombres de cliente")
+    print("4.- Calcular ganancias")
+    print("5.- Salir del programa")
+    opcion = input("Seleccione opción:")
+
+    if opcion not in listadoDeOpciones:
+        print("=== Error: Opción no válida")
+        input("Presione enter para continuar...")
+        continue
+    if opcion == "5":
+        print("Adios!!!   :)")
+    else:
+        if opcion == "1":
+            arrendar(casillero)
+        elif opcion == "2":
+            mostrarUbicaciones(casillero)
+        elif opcion == "3":
+            mostrarNombre(casillero)
+        elif opcion == "4":
+            calcularGanancias(casillero)
